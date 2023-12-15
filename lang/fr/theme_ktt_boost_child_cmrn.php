7c4283d85655fded22b3028e0f74dcbeab48f97b
Joaquin Urbano Serra <kimbalear@gmail.com>
2023-12-15 17:07:42 +0100

    update
    Multilingual file for French language - Child theme section (General Settings) translated - translation of static pages: About, contact (Edwiser plugin)

---
+++ lang/fr/theme_ktt_boost_child_cmrn.php
@@ -0,0 +1,54 @@
+<?php
+// Every file should have GPL and copyright in the header - we skip it in tutorials but you should not skip it for real.
+
+// This line protects the file from being accessed by a URL directly.
+defined('MOODLE_INTERNAL') || die();
+
+// A description shown in the admin theme selector.
+$string['choosereadme'] = "Le thème ktt_boost_child_cmrn est un thème enfant de Boost. Il ajoute la possibilité de télécharger des photos d'arrière-plan.";
+// The name of our plugin.
+$string['pluginname'] = "thème ktt_boost_child_cmrn";
+// We need to include a lang string for each block region.
+$string['region-side-pre'] = 'Droit';
+// The name of the second tab in the theme settings.
+$string['advancedsettings'] = 'Réglages avancés';
+// The brand colour setting.
+$string['brandcolor'] = 'Couleur de la marque';
+// The brand colour setting description.
+$string['brandcolor_desc'] = 'La couleur d’accent.';
+// Name of the settings pages.
+$string['configtitle'] = 'Paramètres de ktt_boost_child_cmrn';
+// Name of the first settings tab.
+$string['generalsettings'] = 'Réglages généraux';
+// Preset files setting.
+$string['presetfiles'] = 'Fichiers prédéfinis de thème supplémentaires';
+// Preset files help text.
+$string['presetfiles_desc'] = "Les fichiers prédéfinis peuvent être utilisés pour modifier considérablement l'apparence du thème. Voir <a href=https://docs.moodle.org/dev/Boost_Presets>Boostez les préréglages</a> pour plus d'informations sur la création et le partage de vos propres fichiers prédéfinis, et consultez le <a href=http://moodle.net/boost>Référentiel de préréglages</a> pour les préréglages que d’autres ont partagés.";
+// Preset setting.
+$string['preset'] = "Thème prédéfini";
+// Preset help text.
+$string['preset_desc'] = "Choisissez un préréglage pour modifier considérablement l’apparence du thème.";
+// Raw SCSS setting.
+$string['rawscss'] = 'SCSS brut';
+// Raw SCSS setting help text.
+$string['rawscss_desc'] = 'Utilisez ce champ pour fournir le code SCSS ou CSS qui sera injecté à la fin de la feuille de style.';
+// Raw initial SCSS setting.
+$string['rawscsspre'] = 'SCSS initial brut';
+// Raw initial SCSS setting help text.
+$string['rawscsspre_desc'] = "Dans ce champ vous pouvez fournir le code SCSS d'initialisation, il sera injecté avant tout. La plupart du temps, vous utiliserez ce paramètre pour définir des variables.";
+
+//custom page
+$string['contact_title'] = 'Titre du contact';
+$string['contact_heading'] = 'Encabezado du contact';
+
+$string['about_title'] = "Titre de la à propos";
+$string['about_heading'] = 'Encabezado de la about';
+
+$string['privacy_title'] = 'Titre de la vie privée';
+$string['privacy_heading'] = 'Encabezado de la vie privée';
+
+$string['terms_title'] = 'Encabezado de la vie privée';
+$string['terms_heading'] = 'Encabezado des termes';
+
+$string['service_title'] = 'Titre du service';
+$string['service_heading'] = 'Encabezado du service';
